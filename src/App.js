324be9242c583127c26c3895fa41a831e2630135
Yanquiel Arango Gomez <yanquielwebdesigner@gmail.com>
2021-02-13 16:36:02 +0100

    my app movie

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import Movie from './components/Movie';
+import axios from 'axios';
 
-function App() {
+const URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1cd94c5adb83441b100d72a4f5f121d7';
+
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=1cd94c5adb83441b100d72a4f5f121d7&query=";
+
+
+const App = () => {
+  const [ movies, setMovies ] = useState([]);
+  const [ search, setSearch ] = useState('');
+
+
+  useEffect( () => {
+    const getMovies = async () => {
+    
+    const resultado = await axios(URL);
+    console.log(resultado.data.results);
+    setMovies(resultado.data.results);
+  }
+    
+    getMovies();
+    
+  },[]);
+
+  const handleOnSubmit = (e) => {
+      e.preventDefault();
+
+      if(search) {
+
+      const getSearch = async () => {
+    
+    const resultado = await axios(SEARCH_API + search);
+    console.log(resultado.data.results);
+    setMovies(resultado.data.results);
+  }
+    
+    getSearch();
+    setSearch('');
+    };
+  }
+
+  const handleOnChange = (e) => {
+    setSearch(e.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h3>MyMovies</h3>
+        <form onSubmit={handleOnSubmit}>
+          <input type="text"
+            className="search"
+            placeholder="Search..."
+            value={search}
+            onChange={handleOnChange}/>
+        </form>
       </header>
-    </div>
-  );
+        <div className="movie-container">
+            {movies.length >0 && movies.map((movie) => (
+              <Movie key={movie.id} {...movie}/>
+            ))} 
+        </div>
+    </>
+  )
 }
 
-export default App;
+export default App
